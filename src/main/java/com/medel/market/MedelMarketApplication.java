96cd8caa51e89d985b9a8fff018cdd357b251ffc
FelipeMedel <luispipemedel@gmail.com>
2020-10-30 15:12:22 -0500

    Creación del Proyecto

---
+++ src/main/java/com/medel/market/MedelMarketApplication.java
@@ -0,0 +1,13 @@
+package com.medel.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedelMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedelMarketApplication.class, args);
+	}
+
+}
